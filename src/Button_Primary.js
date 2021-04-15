0e5699f3161138583a5602d2285e09a1873a7f2a
Fernanda Nardoni <thefray.br@gmail.com>
2021-04-14 22:32:19 -0300

    a few changes

--- src/Button_Primary.js
+++ src/Button_Primary.js
@@ -1,7 +1,24 @@
 import styled from 'styled-components';
 import Button from './Button';
 
+const sm = '70px';
+const lg =  '90px';
+const md = '80px';
+
+const smH = '30px';
+const lgH =  '50px';
+const mdH = '40px';
+
 const Button_Primary = styled(Button)`
+  width: ${ props => props.sm ? sm : undefined };
+  width: ${ props => props.md ? md : undefined };
+  width: ${ props => props.lg ? lg : undefined };
+
+  height: ${ props => props.sm ? smH : undefined };
+  height: ${ props => props.md ? mdH : undefined };
+  height: ${ props => props.lg ? lgH : undefined };
+
+
   background: ${props => props.outline || props.transparent ? "transparent" : "#3D5AFE"};
   color: ${props => props.outline || props.transparent ? "#3D5AFE" : "white"};
   border: ${props => props.outline ? "2px solid #3D5AFE" : "transparent"};
--- src/index.js
+++ src/index.js
@@ -2,9 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import Button from './Button';
-import Button_Primary from './Button_Primary'
-import Button_Secondary from './Button_Secondary'
-import Button_Danger from './Button_Danger'
+import Button_Primary from './Button_Primary';
+import Button_Secondary from './Button_Secondary';
+import Button_Danger from './Button_Danger';
 
 
 
@@ -14,7 +14,9 @@ ReactDOM.render(
 
     <Button> Default </Button>
     <Button_Primary transparent> Transparent </Button_Primary>
-    <Button_Primary> Primary </Button_Primary>
+    <Button_Primary sm> Small </Button_Primary>
+    <Button_Primary md> Primary </Button_Primary>
+    <Button_Primary lg> Large </Button_Primary>
     <Button_Primary outline> Primary Outline </Button_Primary>
     <Button_Secondary> Secondary </Button_Secondary>
     <Button_Secondary secondaryOutline> Secondary Outline </Button_Secondary>
